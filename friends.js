ae56d952a5b9e110d7d3a638937513916d693cbb
MarsCampbell <johnaj22@hotmail.com>
2018-10-05 19:22:19 -0400

    Create friends.js

---
+++ friends.js
@@ -0,0 +1,215 @@
+friends = [
+
+    {
+      "name": "Georgie Porgie",
+      "photo": "",
+      "scores": [
+        3,
+        4,
+        2,
+        2,
+        3,
+        1,
+        3,
+        5,
+        5,
+        3,
+        2
+      ]
+    },
+    {
+      "name": "Clarisa Boyd",
+      "photo": "",
+      "scores": [
+        2,
+        4,
+        1,
+        5,
+        2,
+        5,
+        1,
+        3,
+        4,
+        1
+      ]
+    },
+    {
+      "name": "Jordan Maxwell",
+      "photo": "",
+      "scores": [
+        2,
+        1,
+        5,
+        5,
+        2,
+        2,
+        5,
+        1,
+        3,
+        5
+      ]
+    },
+    {
+      "name": "Kate Caskenette",
+      "photo": "",
+      "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+      ]
+    },
+    {
+      "name": "Calvin Broom",
+      "photo": "",
+      "scores": [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+      ]
+    },
+    {
+      "name": "Ashley Fischer",
+      "photo": "",
+      "scores": [
+        5,
+        3,
+        3,
+        4,
+        1,
+        4,
+        2,
+        5,
+        2,
+        4
+      ]
+    },
+    {
+      "name": "Cole Dougherty",
+      "photo": "",
+      "scores": [
+        2,
+        3,
+        1,
+        5,
+        2,
+        4,
+        2,
+        4,
+        2,
+        5
+      ]
+    },
+    {
+      "name": "Terry Fox",
+      "photo": "",
+      "scores": [
+        4,
+        3,
+        4,
+        5,
+        2,
+        3,
+        5,
+        2,
+        2,
+        5
+      ]
+    },
+    {
+      "name": "Taylor Swift",
+      "photo": "",
+      "scores": [
+        4,
+        3,
+        3,
+        2,
+        2,
+        1,
+        3,
+        3,
+        3,
+        5
+      ]
+    },
+    {
+      "name": "Chris Hemsworth",
+      "photo": "",
+      "scores": [
+        4,
+        3,
+        3,
+        2,
+        2,
+        1,
+        3,
+        3,
+        3,
+        5
+      ]
+    },
+    {
+      "name": "Lucy Rigby",
+      "photo": "",
+      "scores": [
+        3,
+        2,
+        4,
+        2,
+        1,
+        1,
+        5,
+        3,
+        5,
+        5
+      ]
+    },
+    {
+      "name": "Joe Smith",
+      "photo": "",
+      "scores": [
+        1,
+        5,
+        1,
+        5,
+        1,
+        5,
+        1,
+        5,
+        1,
+        5
+      ]
+    },
+    {
+      "name": "Kanye West",
+      "photo": "",
+      "scores": [
+        5,
+        1,
+        5,
+        1,
+        5,
+        1,
+        5,
+        1,
+        5,
+        1
+      ]
+    }
+  
+  ]
+
+  module.exports=friends;
