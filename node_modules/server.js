1aa64117e1452de9a8c8d0e335792dd38ead402d
MarsCampbell <johnaj22@hotmail.com>
2018-10-11 17:51:25 -0400

    Add files via upload

---
+++ node_modules/server.js
@@ -0,0 +1,18 @@
+//required dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var htmlRoutes = require ("./routing/htmlRoutes");
+var apiRoutes = require ("./routing/apiRoutes");
+var app = express();
+var port = process.env.PORT || 3000;
+
+//app routes
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(htmlRoutes);
+app.use(apiRoutes);
+app.use(express.static('public'));
+app.listen(port);
+
+//listener writing out to the console
+console.log("Listen on: " + port);
---
+++ routing/apiRoutes.js
@@ -0,0 +1,15 @@
+var express = require ("express");
+var router = express.Router();
+var friends = require ("../app/friends");
+
+    router.get("/app/friends" ,function(request, response){
+        response.send(friends);
+    })
+    
+    router.post("/app/friends", function(request, response) {    
+        friends.push(request.body);  
+        response.json(true);
+    });
+      
+      
+module.exports = router;
